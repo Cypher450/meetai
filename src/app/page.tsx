32c9cc3a9dd4daa3ddca9c47be30bcd2601976e6
Cypher450 <seshdev01012001@gmail.com>
2025-10-26 22:13:11 +0530

    01: Setup

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+import { Button, buttonVariants } from "@/components/ui/button"
+import { buttonGroupVariants } from "@/components/ui/button-group"
+
+export default function Home() {
+  return (
+    <Button variant= "seshadev">Click me</Button>
+  )
+}
