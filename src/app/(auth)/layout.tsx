65eea28c6f49e1deea733eff2b5072b9bc2e3007
Seshadev_Sahoo <seshdev01012001@gmail.com>
2025-11-09 21:30:11 +0530

    3 auth UI

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,18 @@
+
+
+interface Props {
+    children: React.ReactNode;  
+};
+
+
+const layout = ({children} : Props) => {
+  return ( 
+            <div className="flex items-center justify-center min-h-screen">
+                <div className="text-2xl font-semibold"> 
+                    {children}
+                </div> 
+            </div>  
+        );
+};
+
+export default layout;
